16a64cdefa6fb4f6359e6a5fffde4a99c5cd75aa
Aleksandr Pogrebitskiy <a.pogr@yandex.ru>
2021-02-07 18:12:11 +0300

    Add structure for board

---
+++ src/components/Board.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="board-wrapper">
+    <div class="board">
+      <BoardItem v-for="i in 25" :key="'item-' + i" />
+    </div>
+    
+    <p class="difficult">Сложность: <strong>{{ difficult }}</strong></p>
+  
+    <button class="btn">Старт</button>
+  </div>
+</template>
+
+<script>
+import BoardItem from './BoardItem';
+import { ref } from 'vue';
+
+export default {
+  name: 'Board',
+  props: {},
+  components: {
+    BoardItem,
+  },
+  setup() {
+    const difficult = ref(3);
+    
+    return {
+      difficult
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .board-wrapper {
+    margin-bottom: 100px;
+  }
+  
+  .board {
+    width: 300px;
+    background: #eee;
+    margin: 0 auto;
+  }
+  
+  .btn {
+    background: #42b983cc;
+    color: white;
+    border: none;
+    border-radius: 2px;
+    padding: 10px 30px;
+    margin: 10px 0;
+    cursor: pointer;
+    outline: none;
+  }
+  
+  button:hover {
+    background: #42b983;
+  }
+</style>
---
+++ src/components/BoardItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <span class="item"></span>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+  .item {
+    position: relative;
+    width: 50px;
+    height: 50px;
+    background: #ccc;
+    margin: 5px;
+    display: inline-block;
+    cursor: pointer;
+  }
+</style>
